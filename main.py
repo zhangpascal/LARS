75a205aa136d6ecef265fa808321b505642364b3
zhangpascal <zhang.pascal.yh@gmail.com>
2024-05-18 21:12:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,15 @@
+from sklearn.datasets import make_regression
+from sklearn.datasets import load_diabetes
+import lars 
 import numpy as np
 
-def pre_proc(X,y):
-    X = np.array(X)
-    y = np.array(y)
-    
-    X = X-np.mean(X, axis=0)
-    X = X/np.linalg.norm(X, axis=0)
-    y = y-np.mean(y)
-    
-    return X, y
+reg = lars.LARS()
 
-def my_lars(X,y):
-    
-    n = len(X)
-    p = len(X[0])
-      
-    X_active = np.array([[] for i in range(n)])
-    
-    mu = np.zeros(n)
-    
-    for i in range(p):
-        
-        c = np.dot(X.T,y-mu)
-        
-        c_max_ind = np.argmax(np.absolute(c))
-        c_sign = np.sign(c[c_max_ind])
-        c_max = np.absolute(c[c_max_ind])
+X, y = make_regression(n_samples = 1000000, n_features = 100, n_informative= 100, noise=0.6)
+X, y = lars.pre_proc(X, y)
 
-        X_active = np.append(X_active, c_sign*X[:,c_max_ind].reshape(-1,1), axis=1)
-        X = np.delete(X, c_max_ind, 1)
-        
-        g_inv=np.linalg.inv(np.dot(X_active.T,X_active))
+reg.fit(X, y)
+R2 = 1-np.sum((y-np.dot(X,reg.coef))**2)/np.sum((y-np.mean(y))**2)
+print(reg.time)
+print(R2)
 
-        a_max =1/np.sqrt(np.sum(g_inv))
-        
-        u = np.dot(X_active,np.sum(a_max*g_inv,axis = 1))
-        
-        c_inactive =  np.delete(c,c_max_ind)
-        a_inactive = np.dot(X.T, u)
-        
-        if i < p-1:
-            gamma1 = (c_max - c_inactive)/(a_max - a_inactive)
-            gamma2 = (c_max + c_inactive)/(a_max + a_inactive)
-            gamma = np.min([gamma1[gamma1>=0], gamma2[gamma2>=0]])
-        else:
-            gamma = c_max/a_max
-        
-        mu = mu + gamma*u
-        
-    return mu
-
-X, y = pre_proc(X, y)
-
-mu = my_lars(X,y)
-beta = np.dot(np.linalg.inv(X[:len(X[0]),:]),mu[:len(X[0])])
-
-print(beta)
