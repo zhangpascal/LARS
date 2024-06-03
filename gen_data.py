0ad16de7aa8ee2bff11313a2755f78fd2ddb2eed
Pascal <kalezhang@hotmail.com>
2024-06-03 11:12:27 +0200

    random beta

--- gen_data.py
+++ gen_data.py
@@ -1,12 +1,27 @@
 import numpy as np
 
-def gen_data(n, p, seed):
+def pre_proc(X):
+    X = np.array(X)
+    
+    m = np.mean(X, axis=0)
+    std = np.std(X, axis=0)
+    
+    X = (X-m)/std
+    return X
+
+def gen_data(n, p, num_b, seed):
+    
+    mean_mat = np.zeros(p)
+    cov_mat = np.eye(p)
+
     rng = np.random.default_rng(seed)
-    X = rng.normal(0, 1, (n, p))
+    X = rng.multivariate_normal(mean_mat, cov_mat, n)
+    
+    X = pre_proc(X)
     
     beta = np.zeros(p)  #beta vector
     
-    beta[np.arange(0,3,1)] = 1  #selection of beta 
+    beta[rng.choice(p, num_b, replace = False)] = 1  #selection of beta 
     
     y = X@beta
     
--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import numpy as np
 from gen_data import gen_data
+from sklearn import linear_model as lin_mod
 
-n = 10
-p = 20
+n = 300
+p = 1000
+num_b = 10
 seed = 42
 
-X, y, beta = gen_data(n, p, seed)
+X, y, beta = gen_data(n, p, num_b, seed)
 
-print(X)
-print(y)
+reg = lin_mod.Lars()
+reg.fit(X, y)
+
+a = reg.coef_>0
+b = beta>0
+
+#print(a, b)
+
+print(np.sum((a == b)))
+
+
