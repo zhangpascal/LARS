4ba3699f529506058a0e26b832a69d945ac9514f
Pascal <kalezhang@hotmail.com>
2024-06-04 14:33:16 +0200

    add Pearson corr

--- gen_data.py
+++ gen_data.py
@@ -9,10 +9,21 @@ def pre_proc(X):
     X = (X-m)/std  
     return X
 
-def gen_data(n, p, num_b, seed):
+def gen_data(n, p, num_b, corr, perc, seed):
     
     mean_mat = np.zeros(p) 
     cov_mat = np.eye(p)
+    
+    nb_corr = round(p*perc)
+    
+    #subset of correlated covariates
+    
+    all_index = np.array([[i,j] for i in range(p) for j in range(i)])
+    corr_index = all_index[np.random.choice(len(all_index) , nb_corr, replace=False)]
+    
+    for i,j in corr_index:  
+        cov_mat[i, j] = corr
+        cov_mat[j, i] = corr
 
     rng = np.random.default_rng(seed) #reproducibility
     X = rng.multivariate_normal(mean_mat, cov_mat, n) #data 
--- main.py
+++ main.py
@@ -2,20 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 from gen_data import gen_data
 from sklearn import linear_model as lin_mod
+from sklearn.metrics import confusion_matrix
 
 
 n = 500
 p = 1000
-num_b = 40
+num_b = 50
 seed = 42
+lst = []
+Corr= [0.5, 0.6, 0.7, 0.8, 0.85, 0.9, 0.95, 0.99, 1]
+Perc= [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
 
-X, y, beta = gen_data(n, p, num_b, seed)
+for corr in Corr:
+    for perc in Perc:
+        X, y, beta = gen_data(n, p, num_b, corr, perc, seed)
 
-reg = lin_mod.Lars()
-reg.fit(X, y)
+        reg = lin_mod.Lars()
+        reg.fit(X, y)
 
-a = reg.coef_>0 #selection of actives 
-b = beta>0
+        a = (reg.coef_>0.9)*(reg.coef_<1.1) #selection of actives 
+        b = beta > 0.9
+        
+        tp, fn, fp, tn = confusion_matrix(b,a, labels=[True, False]).ravel()
+            
+        data = {"nb_p": p, "nb_b": num_b, "tp": tp, "fn": fn, "fp": fp, "tn": tn}
+        lst.append(data)
 
 fig = plt.figure()
 #plt.subplot(2,1,1)
